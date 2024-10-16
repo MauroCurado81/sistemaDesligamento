1e8bfcd1341d2b09fa805d8ec0860342bd0ec51d
MauroCurado81 <maurocurado@yahoo.com.br>
2024-10-15 21:58:56 -0300

    Programando Botão Agendar

--- sistemaDesligamento/Form1.Designer.cs
+++ sistemaDesligamento/Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dtAgendar = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAgendar = new System.Windows.Forms.Button();
+            this.txbMinutos = new System.Windows.Forms.TextBox();
+            this.btnAgendarMin = new System.Windows.Forms.Button();
+            this.btnDesliga = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtAgendar
+            // 
+            this.dtAgendar.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtAgendar.Location = new System.Drawing.Point(33, 35);
+            this.dtAgendar.Name = "dtAgendar";
+            this.dtAgendar.Size = new System.Drawing.Size(212, 20);
+            this.dtAgendar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(95, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Escolha o Horário";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAgendar
+            // 
+            this.btnAgendar.Location = new System.Drawing.Point(98, 61);
+            this.btnAgendar.Name = "btnAgendar";
+            this.btnAgendar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgendar.TabIndex = 2;
+            this.btnAgendar.Text = "Agendar";
+            this.btnAgendar.UseVisualStyleBackColor = true;
+            this.btnAgendar.Click += new System.EventHandler(this.btnAgendar_Click);
+            // 
+            // txbMinutos
+            // 
+            this.txbMinutos.Location = new System.Drawing.Point(33, 141);
+            this.txbMinutos.Name = "txbMinutos";
+            this.txbMinutos.Size = new System.Drawing.Size(212, 20);
+            this.txbMinutos.TabIndex = 3;
+            // 
+            // btnAgendarMin
+            // 
+            this.btnAgendarMin.Location = new System.Drawing.Point(98, 167);
+            this.btnAgendarMin.Name = "btnAgendarMin";
+            this.btnAgendarMin.Size = new System.Drawing.Size(75, 23);
+            this.btnAgendarMin.TabIndex = 4;
+            this.btnAgendarMin.Text = "Agendar";
+            this.btnAgendarMin.UseVisualStyleBackColor = true;
+            // 
+            // btnDesliga
+            // 
+            this.btnDesliga.Location = new System.Drawing.Point(102, 268);
+            this.btnDesliga.Name = "btnDesliga";
+            this.btnDesliga.Size = new System.Drawing.Size(75, 23);
+            this.btnDesliga.TabIndex = 5;
+            this.btnDesliga.Text = "Desligar Agora";
+            this.btnDesliga.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(102, 315);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(95, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Desligar daqui à";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(95, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Outros";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(276, 366);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnDesliga);
+            this.Controls.Add(this.btnAgendarMin);
+            this.Controls.Add(this.txbMinutos);
+            this.Controls.Add(this.btnAgendar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtAgendar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker dtAgendar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgendar;
+        private System.Windows.Forms.TextBox txbMinutos;
+        private System.Windows.Forms.Button btnAgendarMin;
+        private System.Windows.Forms.Button btnDesliga;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
